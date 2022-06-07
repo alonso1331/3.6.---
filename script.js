2d29a1dae5a92fce1dbec8008834cf6fb5ac2252
Victoria <odin0330@gmail.com>
2022-06-07 08:07:22 +0800

    已完成初版

---
+++ script.js
@@ -0,0 +1,22 @@
+let header = document.querySelector("header");
+window.addEventListener("scroll", () =>{
+    if(window.pageYOffset !=0 ){
+        header.style = "box-shadow: 3px 3px 5px 1px #757474;";
+    }else{
+        header.style= "box-shadow: none;";
+    }
+});
+
+// function atention(){
+//     document.getElementById("btn").onclick = function (){
+//         window.alert("您確定送出？");
+//     };
+// };
+// atention();
+
+function atention(){
+    document.getElementById("btn").onclick = function (){
+        window.alert("我們將盡速為您處理");
+    };
+};
+atention();
